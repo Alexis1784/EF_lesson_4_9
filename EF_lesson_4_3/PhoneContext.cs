acf944504e2f743983f0dfd1f3f09c98950f37fa
Alex <alexis1784@gmail.com>
2016-01-25 12:17:15 +0500

    working version

---
+++ EF_lesson_4_3/PhoneContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_4_3
+{
+    class PhoneContext : DbContext
+    {
+        static PhoneContext()
+        {
+            Database.SetInitializer(new MyContextInitializer());
+        }
+        public PhoneContext()
+            : base("EF_lessonES")
+        { }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
---
+++ EF_lesson_4_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PhoneContext db = new PhoneContext())
+            {
+                var phones = db.Phones.Where(p => p.Company.Name == "Samsung");
+                foreach (Phone p in phones)
+                    Console.WriteLine("{0}.{1} - {2}", p.Id, p.Name, p.Price);
+            }
+            Console.ReadLine();
+        }
+    }
+}
